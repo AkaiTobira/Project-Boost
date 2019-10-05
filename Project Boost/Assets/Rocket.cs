247d4d7b0733c80685bb17d753f724bc5890b32c
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2019-10-05 14:27:15 +0200

    Create Rocket.cs

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput(){
+        
+        if( Input.GetKey( KeyCode.Space )){
+            rigidbody.AddRelativeForce(Vector3.up * 100 * Time.deltaTime);
+        }
+        if( Input.GetKey( KeyCode.A )){
+            transform.Rotate(Vector3.forward * 50 * Time.deltaTime);
+
+        } else if( Input.GetKey( KeyCode.D )){
+            transform.Rotate(-Vector3.forward * 50 * Time.deltaTime);
+        }
+    }
+}
